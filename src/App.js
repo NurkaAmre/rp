e9fa14e5271e15c331ba42295498e7fa27e23ae1
NurkaAmre <nurkerey30@gmail.com>
2025-04-12 21:38:02 +0200

    Style the card

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import './index.css';
+
 function App() {
   return (
     <div className="card">
@@ -17,12 +19,36 @@ function Avatar() {
 function Intro() {
   return (
     <>
-    <h1>Nurgul</h1>
-    <p>Full-stack web developer and UI/UX designer. When not coding or preparing yutube videos, I like to teach my son for new skills and learning French, dance or walk with my son outside</p>
+      <h1>Nurgul</h1>
+      <p>
+        Full-stack web developer and UI/UX designer. When not coding or
+        preparing yutube videos, I like to teach my son for new skills and
+        learning French, dance or walk with my son outside
+      </p>
     </>
-  )
+  );
 }
 
-function SkillList() {}
+function SkillList() {
+  return (
+    <div>
+      <Skill name="HTML CSS" emoji="🔥" bg="#ccc" />
+      <Skill name=" Javascript" emoji="👍🏻" />
+      <Skill name="Web Design" emoji="🔥" bg="#ccc" />
+      <Skill name="Git and Github" emoji="🔥" bg="#ccc" />
+      <Skill name="React" emoji="🔥" bg="#ccc" />
+      <Skill name="Node js" emoji="🔥" bg="#ccc" />
+    </div>
+  );
+}
+
+function Skill({ name, emoji, bg }) {
+  return (
+    <div className={bg}>
+      <p>{name}</p>
+      <span>{emoji}</span>
+    </div>
+  );
+}
 
 export default App;
