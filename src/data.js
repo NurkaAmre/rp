b08cb6102a94e19f4d49cd709b47179382993804
NurkaAmre <nurkerey30@gmail.com>
2025-04-18 11:15:36 +0200

    Create data file

---
+++ src/data.js
@@ -0,0 +1,40 @@
+const data = [
+  {
+    skill: 'HTML',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg',
+    color: '#E34F26',
+    level: 'Advanced',
+  },
+  {
+    skill: 'CSS',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg',
+    color: '#1572B6',
+    level: 'Advanced',
+  },
+  {
+    skill: 'JavaScript',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
+    color: '#F7DF1E',
+    level: 'Advanced',
+  },
+  {
+    skill: 'React',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
+    color: '#61DAFB',
+    level: 'Intermediate',
+  },
+  {
+    skill: 'Node.js',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg',
+    color: '#339933',
+    level: 'Intermediate',
+  },
+  {
+    skill: 'Git',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg',
+    color: '#F05032',
+    level: 'Intermediate',
+  },
+];
+
+export default data;
